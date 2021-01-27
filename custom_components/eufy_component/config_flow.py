51a9241fd872052f35cffe5d56b08942ae374cf4
Rihan <aleatza@gmail.com>
2021-01-27 15:23:10 +0100

    fix vol schema

--- custom_components/eufy_component/config_flow.py
+++ custom_components/eufy_component/config_flow.py
@@ -1,8 +1,12 @@
 from homeassistant import config_entries
-from .const import DOMAIN
+from .const import DOMAIN, /
+    PASSWORD, EMAIL, /
+    TFA_NONE, TFA_EMAIL, TFA_SMS, TFA_NOTIFICATION
+
 import voluptuous as vol
+import logging
 
-
+_LOGGER = logging.getLogger(__name__)
 class LoginFlow(config_entries.ConfigFlow, domain=DOMAIN):
     
     async def async_step_user(self, info):
@@ -11,8 +15,11 @@ class LoginFlow(config_entries.ConfigFlow, domain=DOMAIN):
         else:
             return self.async_show_form(
                 step_id="user", data_schema=vol.Schema({
-                    vol.Required("Email"): str
-                    vol.Required("password"): str
-                    vol.Required("two factor autentication"): ['None', 'Email', 'Sms', 'Notification']
+                    vol.Required("Email"): str, 
+                    vol.Required("password"): str,
+                    vol.Required("two factor autentication"): vol.In([TFA_NONE, TFA_EMAIL, TFA_SMS, TFA_NOTIFICATION])
                 })
             )
+    
+    async def _config(self, info):
+        _LOGGER.warn(info)
--- custom_components/eufy_component/const.py
+++ custom_components/eufy_component/const.py
@@ -1,1 +1,9 @@
 DOMAIN = "eufy_component"
+
+EMAIL = 'Email'
+PASSWORD = 'Password'
+
+TFA_NONE = 'None'
+TFA_EMAIL = 'Email'
+TFA_SMS = 'Sms'
+TFA_NOTIFICATION = 'Notification'

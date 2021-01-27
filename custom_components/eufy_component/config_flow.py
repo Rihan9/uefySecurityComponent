e83cf726f336642bfa19963d7edc6ccb4a218cb8
Rihan <aleatza@gmail.com>
2021-01-27 18:05:53 +0100

    reformat config flow. add error messages

--- custom_components/eufy_component/config_flow.py
+++ custom_components/eufy_component/config_flow.py
@@ -9,12 +9,39 @@ import logging
 
 _LOGGER = logging.getLogger(__name__)
 class LoginFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    def __init__(self):
-        super.__init__()
-        self.eufyApi = None
+    # def __init__(self):
+    #     super.__init__()
+    #     self.eufyApi = None
     async def async_step_user(self, info):
-        if(info is not None):
-            return await self.async_step_tfa(info)
+        if('eufyApi' not in self.__dict__):
+            self.eufyApi = None
+        if(info is not None and info.get(VERIFICATION_CODE)):
+            return self.async_abort(reason="not_supported")
+        elif(info is not None):
+            if(info.get(TFA) == TFA_NONE):
+                info.set(TFA, TFA_EMAIL)
+            self.eufyApi = Api(
+                username=info.get(EMAIL), 
+                password=info.get(PASSWORD), 
+                preferred2FAMethod=info.get(TFA)
+            )
+            try:
+                response = self.eufyApi.authenticate()
+                if(response == 'OK'):
+                    return self.async_create_entry(title="Eufy Security", data={
+                        TOKEN: self.eufyApi.token,
+                        TOKEN_EXPIRE_AT: self.eufyApi.token_expire_at,
+                        DOMAIN: self.eufyApi.domain
+                    })
+                elif('send_verify_code'):
+                    return self.async_show_form(
+                    step_id="user", data_schema=vol.Schema({
+                        vol.Required(VERIFICATION_CODE): str
+                    })
+                )
+            except LoginException as e:
+                _LOGGER.exception(e)
+                return self.async_abort(reason="login_error")
         else:
             await self.async_set_unique_id(DUMMY_DEVICE_ID)
             self._abort_if_unique_id_configured()
@@ -26,32 +53,3 @@ class LoginFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     vol.Required(TFA): vol.In([TFA_NONE, TFA_EMAIL, TFA_SMS, TFA_NOTIFICATION])
                 })
             )
-    
-    async def async_step_tfa(self, info):
-        if(info.get(VERIFICATION_CODE)):
-            return self.async_abort(reason="not_supported")
-            # return True
-        if(info.get(TFA) == TFA_NONE):
-            info.set(TFA, TFA_EMAIL)
-        self.eufyApi = Api(
-            username=info.get(EMAIL), 
-            password=info.get(PASSWORD), 
-            preferred2FAMethod=info.get(TFA)
-        )
-        try:
-            response = self.eufyApi.authenticate()
-            if(response == 'OK'):
-                return self.async_create_entry(title="Eufy Security", data={
-                    TOKEN: self.eufyApi.token,
-                    TOKEN_EXPIRE_AT: self.eufyApi.token_expire_at,
-                    DOMAIN: self.eufyApi.domain
-                })
-            elif('send_verify_code'):
-                return self.async_show_form(
-                step_id="tfa", data_schema=vol.Schema({
-                    vol.Required(VERIFICATION_CODE): str
-                })
-            )
-        except LoginException as e:
-            _LOGGER.exception(e)
-            return self.async_abort(reason="login_error")

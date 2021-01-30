616c0f55b4e814d9511caad00606284795d1f7b7
Rihan <aleatza@gmail.com>
2021-01-30 20:45:41 +0100

    ccorrect stupid error, use last version of api

--- custom_components/eufy_security/utils.py
+++ custom_components/eufy_security/utils.py
@@ -4,9 +4,9 @@ def wrap(device, attribute):
 
     switch = {
         PARAM_TYPE.GUARD_MODE:
-            lambda : GUARD_MODE(device.attribute[attribute]).name.lower.replace('_', ' ') 
+            lambda : GUARD_MODE(device.attribute[attribute]).name.lower().replace('_', ' ') 
                 if device.attribute[attribute] != GUARD_MODE.SCHEDULED.value 
-                else 'scheduled mode: ' + GUARD_MODE(device.attribute[PARAM_TYPE.SCHEDULE_MODE]).name.lower.replace('_', ' ') 
+                else 'scheduled mode: ' + GUARD_MODE(device.attribute[PARAM_TYPE.SCHEDULE_MODE]).name.lower().replace('_', ' ') 
     }
     if(attribute in switch):
         return switch[attribute]()
